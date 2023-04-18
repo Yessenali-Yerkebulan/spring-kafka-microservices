c029f866790b2324a29d46bd9961d37ef3fb8c27
Yerkebulan <yerkebulan.yessenali@narxoz.kz>
2023-04-18 15:33:25 +0600

    configured and created Kafka Consumer in EmailService

---
+++ email-service/src/main/java/kz/yessenali/emailservice/kafka/OrderConsumer.java
@@ -0,0 +1,23 @@
+package kz.yessenali.emailservice.kafka;
+
+import kz.yessenali.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class OrderConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics="${spring.kafka.topic.name}",
+    groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent event){
+        LOGGER.info(String.format("Order event received in email service => %s", event.toString()));
+
+
+
+
+    }
+}
