88a5c4c4fb4f112b9cbe655c36f4b9ead4259811
Yerkebulan <yerkebulan.yessenali@narxoz.kz>
2023-04-18 10:08:49 +0600

    created Order and OrderEvent DTO Classes in Base Domains

---
+++ base-domains/src/main/java/kz/yessenali/basedomains/dto/OrderEvent.java
@@ -0,0 +1,14 @@
+package kz.yessenali.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+    private String message;
+    private String status;
+    private Order order;
+}
